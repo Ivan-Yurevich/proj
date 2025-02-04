d27ff489362af8f30f78da5c072250727ca9362d
Ivan Yurevich <marcusxakep@gmail.com>
2025-02-04 20:11:52 +0300

    Улучшили логику main

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -1,12 +1,13 @@
 public class MainApplication {
     public static void main(String[] args) {
+        System.out.println("--------");
         System.out.println("Hello, World");
         System.out.println("1111111");
     }
 
     public static void doSomething() {
+        System.out.println(0);
         System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
+        System.out.println(1);
     }
 }
