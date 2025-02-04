53af6c0d1a4d194446a9f50797dec385efc34e1d
Ivan Yurevich <marcusxakep@gmail.com>
2025-02-04 19:40:13 +0300

    Изменен метод doSomething

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -3,4 +3,10 @@ public class MainApplication {
         System.out.println("Hello, World");
         System.out.println("1111111");
     }
+
+    public static void doSomething() {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+    }
 }
